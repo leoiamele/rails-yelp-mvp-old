c03d7d3a98d3e769ad606fbac0059d88824c111b
Leandro Iamele <leandroiamele@gmail.com>
2019-07-10 13:14:41 -0300

    bla

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  TYPE = %w[chinese italian japanese french belgian]
+  validates :name, :address, :phone_number, presence: true
+  validates :category, inclusion: { in: [TYPE] }
+  has_many :review, dependent: :destroy
 end
