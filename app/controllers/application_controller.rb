baa0d718295113c08839f65e6aec4ff7b5b17c9a
Leandro Iamele <leandroiamele@gmail.com>
2019-07-11 10:54:13 -0300

    Restaurant proyect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurants_params)
+    @restaurant.save
+    redirect_to @restaurant
+  end
+
+  private
+
+  def restaurants_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
